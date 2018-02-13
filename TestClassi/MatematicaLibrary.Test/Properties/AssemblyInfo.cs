97ca1859d7f2c5b47cd6354f678263d82ff08786
raulubaldi01 <raulubaldi01@outlook.it>
2018-02-13 14:37:11 +0100

    Modifica

---
+++ TestClassi/MatematicaLibrary.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MatematicaLibrary.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MatematicaLibrary.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5ebd2618-959a-4b59-943f-915f93128bbe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestClassi/MatematicaLibrary/Calcolatrice.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatematicaLibrary
+{
+    public static class Calcolatrice
+    {
+        public static double Somma(double a,double b)
+        {
+            double ris = 0;
+            ris = a + b;
+            return ris;
+        }
+        public static double Divisione(double a, double b)
+        {
+            double ris = 0;
+            if (b != 0)
+            {
+                ris = a / b;
+            }
+            else
+            {
+                ris = double.NaN;
+            }
+
+            return ris;
+        }
+        public static double Massimo(double a, double b)
+        {
+            double ris = 0;
+            if (a >= b)
+            {
+                ris = a;
+            }
+            else
+            {
+                ris = b;
+            }
+
+            return ris;
+        }
+        public static double Minimo(double a, double b)
+        {
+            double ris = 0;
+            if (a <= b)
+            {
+                ris = a;
+            }
+            else
+            {
+                ris = b;
+            }
+            return ris;
+        }
+        public static double Media(double a, double b)
+        {
+            double ris = 0;
+            ris = (a + b) / 2;
+            return ris;
+        }
+
+
+
+
+
+
+
+        public static double Mediatre(double a, double b, double c)
+        {
+            double ris = 0;
+            ris = (a + b + c) / 3;
+            return ris;
+        }
+
+
+
+
+
+
+        public static double Minimotre(double a, double b, double c)
+        {
+            double ris = 0;
+            if (a < b && a < c)
+            {
+                ris = a;
+            }
+            if (b < a && b < c)
+            {
+                ris = b;
+            }
+            if (c < a && c < b)
+            {
+                ris = c;
+            }
+            if (a == b && b == c)
+            {
+                ris = b;
+            }
+            return ris;
+        }
+
+
+
+
+
+
+
+
+        public static double Massimotre(double a, double b, double c)
+        {
+            double ris = 0;
+            if (a > b && a > c)
+            {
+                ris = a;
+            }
+            if (b > a && b > c)
+            {
+                ris = b;
+            }
+            if (c > a && c > b)
+            {
+                ris = c;
+            }
+            if (a == b && b == c)
+            {
+                ris = b;
+            }
+            return ris;
+        }
+    }
+}
